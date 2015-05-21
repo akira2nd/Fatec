9919f1884dd4516c51963dbb9ac692042ed5e1f6
Celso Akira Mendes Tomisaki <akira.2nd@hotmail.com>
2015-05-20 21:28:34 -0300

    mais alterações

--- Escape_island/src/escape_island.c
+++ Escape_island/src/escape_island.c
@@ -6,58 +6,110 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
-
-
+const int LARGURA_TELA = 1280;
+const int ALTURA_TELA = 720;
 
 int main(void){
     ALLEGRO_DISPLAY *janela = NULL;
+    ALLEGRO_BITMAP *background = NULL;
+    ALLEGRO_BITMAP *arvoreC = NULL;
+    ALLEGRO_BITMAP *menu = NULL;
+    ALLEGRO_BITMAP *coco = NULL;
+    ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
 
     bool sair = false;
     double p = 0.666666667;
 
-    al_init();
-    al_install_keyboard();
+    if(!al_init()){
+        printf("Erro ao iniciar allegro");
+        return 1;
+    }
+    if(!al_install_keyboard()){
+        printf("Erro ao iniciar teclado");
+        return 1;
+    }
 
-        //teclado
-    ALLEGRO_EVENT_QUEUE *evento_fila = al_create_event_queue();
+    if(!al_init_image_addon()){
+            printf("Erro ao iniciar imagens");
+            return 1;
+    }
+
+    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
+    if(!janela){
+        printf("Erro ao abrir janela");
+        return 1;
+    }
+
+    background = al_load_bitmap("img/bg.jpg");
+    if(!background){
+        printf("Erro ao iniciar imagem de fundo");
+        al_destroy_display(janela);
+        return 1;
+    }
+
+    arvoreC = al_load_bitmap("img/arvore-cortar.png");
+    if(!arvoreC){
+        printf("Erro ao iniciar arvoreC");
+        al_destroy_display(janela);
+        return -1;
+    }
+
+    coco = al_load_bitmap("img/coco.png");
+    if(!coco){
+        printf("Erro pra carregar o coco");
+        al_destroy_display(janela);
+        return -1;
+    }
+
+    menu = al_load_bitmap("img/barra_itens.png");
+    if(!menu){
+        printf("Erro ao iniciar bitmap menu");
+        al_destroy_display(janela);
+        return -1;
+    }
+
+//while (!sair){
+    ALLEGRO_EVENT evento;
+
+    //al_draw_bitmap(background,0, 0, NULL);
+    //printf("%lf",al_get_bitmap_width(arvoreC));
+
+    al_draw_scaled_bitmap(background,0,0,al_get_bitmap_width(background),al_get_bitmap_height(background),0,0,al_get_display_width(janela),al_get_display_height(janela),0);
+
+    al_draw_scaled_bitmap(arvoreC,0,0,al_get_bitmap_width(arvoreC),al_get_bitmap_height(arvoreC),620,44,p*al_get_bitmap_width(arvoreC),p*al_get_bitmap_height(arvoreC),0);
+
+    al_draw_scaled_bitmap(menu,0,0,al_get_bitmap_width(menu),al_get_bitmap_height(menu),50,512,p*al_get_bitmap_width(menu),p*al_get_bitmap_height(menu),0);
+
+    al_draw_scaled_bitmap(coco,0,0,al_get_bitmap_width(coco),al_get_bitmap_height(coco),20,5,p*al_get_bitmap_width(coco),p*al_get_bitmap_height(coco),0);
+
+
+    al_flip_display();
+
+    //al_wait_for_event(fila_eventos, &evento);
+
+    //if(evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+        //sair = true;
+
+/*
+    //teclado
+    ; = al_create_event_queue();
 
     if(evento_fila == NULL)
         return 1;
 
     al_register_event_source(evento_fila, al_get_keyboard_event_source());
 
-    janela = al_create_display(1280, 720);
+*/
 
-    al_init_image_addon();
-
-    ALLEGRO_BITMAP *background = al_load_bitmap("img/bg.jpg");
-    ALLEGRO_BITMAP *arvoreC = al_load_bitmap("img/arvore-cortar.png");
-    ALLEGRO_BITMAP *menu = al_load_bitmap("img/barra_itens.png");
-
-
-
-    //al_draw_bitmap(background,0, 0, NULL);
-    al_draw_scaled_bitmap(background,0,0,1920,1080,0,0,al_get_display_width(janela),al_get_display_height(janela),0);
-    //al_draw_bitmap(arvore,0,100,0);
-    //al_draw_scaled_bitmap(arvore1,0,0,al_get_bitmap_height(arvore1),al_get_bitmap_width(arvore1),180,0,al_get_display_width(janela),al_get_display_height(janela),0);
-    al_draw_scaled_bitmap(arvoreC,0,0,208,335,620,44,p*212,p*325,0);
-
-    al_draw_scaled_bitmap(menu,0,0,1715,312,45,512,p*1715,p*312,0);
-
-
-
-    al_flip_display();
-
-    //al_rest(7.0);
-
+    al_rest(7.0);
+//}
     al_destroy_bitmap(background);
     al_destroy_bitmap(arvoreC);
+    al_destroy_bitmap(coco);
     al_destroy_bitmap(menu);
     al_destroy_display(janela);
 
     return 0;
-
 }
 
 /* DESTE PONTO PRA CIMA EDITADO NO SERVIÇO ***********
